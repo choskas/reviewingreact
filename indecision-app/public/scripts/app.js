2f856f6f1f00c6ad7bf0433ef9e63ca06a9452ff
Choskas <choskasdelta@hotmail.com>
2019-10-24 13:46:26 -0500

    Manual Data Binding

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -3,29 +3,135 @@
 console.log('App.js is running!');
 
 //JSX - Javascript XML
+var app = {
+    title: 'Loquesea',
+    subtitle: 'cosachida',
+    options: ['one', 'two']
+};
+
+function appTitleExist(subtitle) {
+    if (subtitle) {
+        return React.createElement(
+            'p',
+            null,
+            'Title: ',
+            subtitle
+        );
+    }
+}
+
+function optionsExist(options) {
+    if (options.length > 0) {
+        return React.createElement(
+            'li',
+            null,
+            options
+        );
+    }
+}
+
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    appTitleExist(app.subtitle),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? 'Here they are' : 'No options'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        optionsExist(app.options)
+    )
+);
+
+var user = {
+    name: 'yoplai',
+    age: 5000,
+    location: 'mi casa'
+};
+
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
+
 var templateTwo = React.createElement(
     'div',
     null,
     React.createElement(
         'h1',
         null,
-        ' GOku '
+        ' ',
+        user.name ? user.name : 'Anonymus',
+        ' '
     ),
-    React.createElement(
+    user.age >= 18 && React.createElement(
         'p',
         null,
-        ' Age: infinito alv '
+        ' Age: ',
+        user.age
     ),
-    React.createElement(
-        'p',
-        null,
-        'Location: mi casa'
-    )
+    getLocation(user.location)
 );
 
-//1. Create a templateTwo
-//2. <div> <h1>NAME<h1> <p>AGE: AGE </p> <p> LOCATION: LOCATION </p>
+var count = 0;
+var addOne = function addOne() {
+    count++;
+    renderCounterApp();
+};
+
+var removeOne = function removeOne() {
+    count--;
+    renderCounterApp();
+};
+
+var reset = function reset() {
+    count = 0;
+    renderCounterApp();
+};
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+var renderCounterApp = function renderCounterApp() {
+    var templateThree = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            'Count: ',
+            count
+        ),
+        React.createElement(
+            'button',
+            { onClick: addOne },
+            '++'
+        ),
+        React.createElement(
+            'button',
+            { onClick: removeOne },
+            '--'
+        ),
+        React.createElement(
+            'button',
+            { onClick: reset },
+            'RESET'
+        )
+    );
+    ReactDOM.render(templateThree, appRoot);
+};
+
+renderCounterApp();
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,19 +1,92 @@
 console.log('App.js is running!')
 
 //JSX - Javascript XML
-const templateTwo= (
-    <div>
-        <h1> GOku </h1>
-        <p> Age: infinito alv </p>
-        <p>Location: mi casa</p>
+const app = {
+    title: 'Loquesea',
+    subtitle: 'cosachida',
+    options: ['one', 'two']
+}
 
+function appTitleExist(subtitle){
+    if (subtitle){
+        return <p>Title: {subtitle}</p>
+    }
+}
+
+function optionsExist(options){
+    if (options.length > 0){
+        return <li>{options}</li> 
+    }
+}
+
+const template = (
+    <div>
+        <h1>{app.title}</h1>
+        {appTitleExist(app.subtitle)}
+        <p>{app.options.length > 0 ? 'Here they are' : 'No options'}</p>
+        <ol>
+            {optionsExist(app.options)}
+        </ol>
     </div>
 )
 
 
-//1. Create a templateTwo
-//2. <div> <h1>NAME<h1> <p>AGE: AGE </p> <p> LOCATION: LOCATION </p>
+const user = {
+    name: 'yoplai',
+    age: 5000,
+    location: 'mi casa'
+}
+
+function getLocation(location){
+    if(location){
+        return <p>Location: {location}</p>
+    
+    }
+}
+
+const templateTwo= (
+    <div>
+        <h1> {user.name ? user.name : 'Anonymus'} </h1>
+
+        {user.age >= 18 && <p> Age: {user.age}</p>}
+        {getLocation(user.location)}
+    </div>
+)
+
+
+let count = 0
+const addOne = () =>{
+     count ++
+     renderCounterApp()
+}
+
+const removeOne = () =>{
+    count --
+    renderCounterApp()
+}
+
+const reset = () => {
+    count = 0
+    renderCounterApp()
+}
+
+
+
 
 const appRoot = document.getElementById('app')
 
-ReactDOM.render(templateTwo, appRoot)
+const renderCounterApp = () => {
+    const templateThree = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne}>++</button>
+            <button onClick={removeOne}>--</button>
+            <button onClick={reset}>RESET</button>
+        </div>
+    )
+    ReactDOM.render(templateThree, appRoot)
+
+}
+
+renderCounterApp()
+
