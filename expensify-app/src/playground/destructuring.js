f2ea0ab0131692b4be562b1163efcc7133d3fd16
Choskas <choskasdelta@hotmail.com>
2019-11-04 16:00:49 -0600

    array destructuring

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -7,10 +7,22 @@ const person = {
     }
 }
 
-const {name, age} = person
+//default value
+const {name = 'Anonymous', age} = person
 
 console.log(`${name}, ${age}`)
 
+//rename variable
 const {city, temp: temperature} = person.location
 
 console.log(`${city}, la temperatura ptos ${temperature}`)
+
+
+//ARRAY DESTRUCTURING
+
+const address = ['lacalle', 'cdmx', 'pais']
+const [street, ciudad, state = 'New YOrk'] = address
+//si solo se quieren algunos valores
+//const [, , state] = address
+
+console.log(`you are in ${street}, ${ciudad}`)
