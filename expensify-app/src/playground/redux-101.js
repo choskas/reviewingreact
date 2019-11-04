ecbd9e7ae10450f1dec7c79dd7fbc665017b7e95
Choskas <choskasdelta@hotmail.com>
2019-11-04 15:38:24 -0600

    dynamic actions

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -4,12 +4,18 @@ const store= createStore((state = { count: 0}, action)=>{
 
     switch (action.type){
         case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
             return{
-                count: state.count + 1
+                count: state.count + incrementBy
             }
         case 'DECREMENT':
+                const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
             return{
-                count: state.count - 1
+                count: state.count - decrementBy
+            }
+        case 'SET':
+            return{
+                count: action.count
             }
         case 'RESET':
             return {
@@ -27,18 +33,26 @@ const store= createStore((state = { count: 0}, action)=>{
     // }
 })
 
-console.log(store.getState())
+const unsubscribe = store.subscribe(()=>{
+    console.log(store.getState())
+})
+
+
 
 store.dispatch(
     {
-        type: 'INCREMENT'
+        type: 'INCREMENT',
+        incrementBy: 5
     
     }
+    
 )
+
 
 store.dispatch(
     {
-        type: 'DECREMENT'
+        type: 'DECREMENT',
+        decrementBy: 10
     }
 )
 
@@ -47,4 +61,9 @@ store.dispatch(
         type: 'RESET'
     }
 )
+
+store.dispatch({
+    type: 'SET',
+    count: 101
+})
 console.log(store.getState())
